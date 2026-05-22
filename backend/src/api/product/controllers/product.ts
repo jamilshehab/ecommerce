e1594d459258b65765cebefee1a7d992888cf3b3
jamilshehab <jamilshehab>
2026-05-22 17:21:46 +0300

    trying to make some fixes

--- backend/src/api/product/controllers/product.ts
+++ backend/src/api/product/controllers/product.ts
@@ -4,4 +4,28 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController('api::product.product', ({ strapi }) => ({
+  async find(ctx) {
+    // Always populate main_image and category for products
+    ctx.query = {
+      ...ctx.query,
+      populate: ctx.query.populate || ['main_image', 'category'],
+    };
+
+    const { data, meta } = await super.find(ctx);
+
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    // Always populate main_image and category for single product
+    ctx.query = {
+      ...ctx.query,
+      populate: ctx.query.populate || ['main_image', 'category'],
+    };
+
+    const response = await super.findOne(ctx);
+
+    return response;
+  },
+}));
