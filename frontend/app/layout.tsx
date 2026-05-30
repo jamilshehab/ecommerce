ff517d3c7754903f5aa14c6828d9fde8853b8d6c
jamilshehab <jamilshehab>
2026-05-30 14:19:11 +0300

    layout fixes

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         <ToastProvider>
           <Header />
           {children}
-
           <SubscribePopup />
           <CartDrawer />
           <Instagram />
